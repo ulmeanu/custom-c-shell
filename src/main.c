4a4a3003f1327d24b49f638c8dc6d5ed6b2299f6
Marius-Alexandru Ulmeanu <ulmeanu.m.cybersec@gmail.com>
2025-01-07 12:54:50 -0500

    Implement ccsReadLine function to handle user input

--- src/main.c
+++ src/main.c
@@ -3,6 +3,40 @@
 #include <unistd.h>
 #include <string.h>
 
+char *ccsReadLine(void){
+    size_t bufsize = 1024;
+    size_t position = 0;
+    char *buffer = malloc(bufsize * sizeof(char));
+    int c;
+
+    if (!buffer) {
+        fprintf(stderr, "ccs: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        c = getchar();
+        if (c == EOF) {
+            exit(EXIT_SUCCESS);
+        } else if (c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += 1024;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer) {
+                fprintf(stderr, "ccs: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
 void ccsLoop(void) {
     char *line;
     char **args;
